19e3490170ddd855dcb8dc54f38c06b42eff5436
Abrar Ahmad <abrar.ahmad@camp1.tkxel.com>
2023-07-12 01:18:00 +0500

    Shop Urls Update

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def search_product(request):
+    return HttpResponse("This is Search Page")
+
+def single_product(request):
+    return HttpResponse("This is single product Page")
+
+def checkout(request):
+    return HttpResponse("This is checkout Page")
+
+def tracking(request):
+    return HttpResponse("This is tracking Page")
