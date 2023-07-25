44d0bd586358f1492ff138bc3d24e321fab9bfaf
Abrar Khawaja <kh.abrar22@gmail.com>
2023-07-26 00:47:39 +0500

    Fetch and display products on shop home page

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+    # params = {'no_of_slides' : nSlides, 'range' : range(1, nSlides), 'product' : products}
+    params = {'product' : products}
+    print(params)
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
